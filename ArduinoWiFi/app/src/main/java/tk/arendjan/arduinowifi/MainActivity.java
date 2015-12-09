ca9461785e3c57b6099850b8ba3df63a1601f523
Arend-Jan van Hilten <arendjan18@gmail.com>
2015-12-09 15:22:45 +0100

    final update android app
    really easy, now the also small part, the android code.

--- ArduinoWiFi/app/src/main/java/tk/arendjan/arduinowifi/MainActivity.java
+++ ArduinoWiFi/app/src/main/java/tk/arendjan/arduinowifi/MainActivity.java
@@ -31,14 +31,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         //This gets the saved IP address, and puts it in the variable, if not set, then it starts the Setting activity.
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         ip = preferences.getString("IP", "NONE");
@@ -91,6 +84,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, Setting.class);
+            startActivity(intent);
             return true;
         }
 
--- ArduinoWiFi/app/src/main/java/tk/arendjan/arduinowifi/Setting.java
+++ ArduinoWiFi/app/src/main/java/tk/arendjan/arduinowifi/Setting.java
@@ -29,14 +29,6 @@ public class Setting extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         final TextView input = (TextView) findViewById(R.id.ipTF);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
